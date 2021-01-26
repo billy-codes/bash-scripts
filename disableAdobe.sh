838fe8936370a2b54040ca4c96c7e2c4a3abf992
Billy <billy.codes@gmail.com>
2021-01-26 23:48:03 +0800

    Made a few syntax corrections

--- disableAdobe.sh
+++ disableAdobe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ADOBE_CCAPP = "~/Library/LaunchAgents/com.adobe.AdobeCreativeCloud.plist"
-ADOBE_UPDATER = "~/Library/LaunchAgents/com.adobe.AAM.Updater-1.0.plist"i
+CCAPP = "~/Library/LaunchAgents/com.adobe.AdobeCreativeCloud.plist"
+UPDATER = "~/Library/LaunchAgents/com.adobe.AAM.Updater-1.0.plist"
 
 echo "Running $0"
 echo "Please Select Option:"
@@ -9,28 +9,24 @@ echo "1. Turn OFF StartUp Entry"
 echo "2. Turn ON StartUp Entry"
 echo "3. DELETE StartUp Entry"
 read OPTION
-
-if [OPTION == 1]
-then
+if [ OPTION == "1" ]; then
 	echo "Killing Adobe Services" 
-	launchctl unload -w $ADOBE_CCAPP
-	launchctl unload -w $ADOBE_UPDATER
+	launchctl unload -w $CCAPP
+	launchctl unload -w $UPDATER
 	echo "Adobe CC Services Successfully DISABLED"
-elif [OPTION == 2]
-then 
+
+elif [ OPTION == 2 ]; then 
 	echo "Turning Adobe CC Services ON"
-	launchctl load -w $ADOBE_CCAPP
-	launchctl load -w $ADOBE_UPDATER
+	launchctl load -w $CCAPP
+	launchctl load -w $UPDATER
 	echo "Adobe CC Services Sucessfully ENABLED"
-elif [OPTION == 3]
-then 
+elif [ OPTION == 3 ]; then 
 	echo "DELETING Startup Entry"
-	rm -rf $ADOBE_CCAPP
-	rm -rf $ADOBE_UPDATER
+	rm -rf $CCAPP
+	rm -rf $UPDATER
 	echo "Successfully DELETED Startup Entries"
 else
 	echo "Invalid Option"
-	break
 fi
 
 
